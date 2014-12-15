b1e0359bf4710b138314c09a42cc7924e2d646f9
mooophy <MoophyAlan@gmail.com>
2014-12-16 03:36:07 +1300

    modified:   1_10.hs

--- 1_10.hs
+++ 1_10.hs
@@ -1,88 +1,76 @@
-{-|
- - Problem 1
- - Find the last element of a list.
- -
- - ? this way causes a linear complexity.Is there a better way
- -}
+err = error "error--99 hasskell problems"
+
+--1 (*) Find the last element of a list.
 last' :: [a] -> a
-last' [] = error "empty list\n"
-last' [x] = x
-last' (_:xs)    =   last' xs
+last' []     = err 
+last' [x]    = x
+last' (_:xs) = last' xs 
 
 
-{-|
- - Problem 2
- - Find the last but one element of a list.
- -}
-the_second_last::[a]->a
-the_second_last []      =   error "too short"
-the_second_last [_]     =   error "too short"
-the_second_last (x:xs)
-    |   length xs == 1  =   x
-    |   otherwise       =   the_second_last xs
+--2 (*) Find the last but one element of a list.
+lastButOne :: [a] -> a
+lastButOne []        = err
+lastButOne [_]       = err
+lastButOne xs        = xs !! (length xs - 2)
 
 
-{-
- - Problem 3
- - Find the K'th element of a list. The first element in the list is number 1.
- -}
-element_at :: [a] -> Int -> a
-element_at li i         =   li !! (i-1)
+--3 (*) Find the K'th element of a list. The first element in the list is number 1
+k'th :: [a] -> Int -> a
+k'th xs k
+    | length xs < k  = err
+    | otherwise      = xs !! (k - 1)
 
 
-{- Problem 4
- - Find the number of elements of a list.
- - -}
-len' :: [a] -> Int 
-len' []         =   0
-len' (x:xs)     =   1 + len' xs
+--4 (*) Find the number of elements of a list.
+length' :: [a] -> Integer
+length' [] = 0
+length' (x:xs) = 1 + length' xs
 
 
-{- Problem 5
- - Reverse a list.
- - -}
+--5 (*) Reverse a list.
 reverse' :: [a] -> [a]
-reverse' []     =   []
---reverse' [x]    =   [x]   -- unnecessary
-reverse' (x:xs) =   reverse' xs ++ [x]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x] 
 
 
-{- Problem 6
- - Find out whether a list is a palindrome. 
- - -}
-is_palindrome :: (Eq a) =>  [a] -> Bool
-is_palindrome li =   li == reverse' li    
+--6 (*) Find out whether a list is a palindrome. A palindrome can be read forward or backward; e.g. (x a m a x).
+isPalindrome :: (Eq a) => [a] -> Bool 
+isPalindrome xs = xs == reverse xs
 
 
-{- Problem 7
- - Flatten a nested list structure.
- - --copied from haskell.org
- - -- need check info for type def
- - -}
+--7 (**) Flatten a nested list structure. @attention : need recap
 data NestedList a = Elem a | List [NestedList a]
 flatten :: NestedList a -> [a]
-flatten (Elem a)    =   [a]
-flatten (List(x:xs))=   flatten x ++ flatten (List xs)
-flatten (List[])    =   []
+flatten (Elem x) = [x]
+flatten (List x) = concatMap flatten x
+--                 ^^^^^^^^^
+--                 Map a function over a list and concatenate the results.
 
 
-{- Problem 8
- - Eliminate consecutive duplicates of list elements.
- - -}
-compress :: Eq a => [a] -> [a] 
-compress (x:ys@(y:_))
-    | x == y    = compress ys
-    | otherwise = x : compress ys
-compress x = x
+--8 (**) Eliminate consecutive duplicates of list elements.
+elimiConsecDups :: (Eq a) => [a] -> [a] 
+elimiConsecDups []     = []
+elimiConsecDups [x]    = [x]
+elimiConsecDups (x:xs) 
+    | x == head (elimiConsecDups xs)  =  elimiConsecDups xs
+    | otherwise                       =  x : elimiConsecDups xs
 
 
-{- Problem 9
- - Pack consecutive duplicates of list elements into sublists. 
- - If a list contains repeated elements they should be placed in separate sublists.
- - -}
+--9 (**) Pack consecutive duplicates of list elements into sublists. 
+--       If a list contains repeated elements they should be placed 
+--       in separate sublists.
 pack :: (Eq a) => [a] -> [[a]]
-pack []     = []
-pack [x]    = [[x]]
-pack (x:xs) =   if x `elem` (head(pack xs))
-                then (x:(head (pack xs))) : (tail(pack xs))
-                else [x]:(pack xs)
+pack []      =  [[]]
+pack [x]     =  [[x]]
+pack (x:xs)  
+    | x == (head.head $ pack xs) = (x : head (pack xs)) : tail (pack xs)
+    | otherwise                  = [x] : pack xs 
+
+
+--10 (*) Run-length encoding of a list. Use the result of problem P09
+--       to implement the so-called run-length encoding data compression
+--       method. Consecutive duplicates of elements are encoded as lists (N E)
+--       where N is the number of duplicates of the element E.
+encode :: (Eq a) => [a] -> [(Int, a)]
+encode xs    =  map lamda (pack xs)
+    where lambda = (\ys -> (length ys, head ys))
