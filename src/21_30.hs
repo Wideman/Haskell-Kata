f0fb71f9b8eaef308b06b11e6c6c4eab10c4125e
mooophy <MoophyAlan@gmail.com>
2014-12-17 02:06:22 +1300

    modified:   21_30.hs

--- src/21_30.hs
+++ src/21_30.hs
@@ -2,8 +2,16 @@ err = error "errors ..."
 
 --Problem 21
 --Insert an element at a given position into a list.
-
 insertAt :: a -> [a] -> Int -> [a]
 insertAt elem [] _ = [elem]
 insertAt elem xs 0 = elem : xs
 insertAt elem (x:xs) n = (:) x $ insertAt elem xs $ n - 1
+
+
+--2 Problem 22
+--Create a list containing all integers within a given range.
+range :: Int -> Int -> [Int]
+range beg end 
+    | beg == end  = []
+    | otherwise   = beg : range (beg + 1) end 
+
