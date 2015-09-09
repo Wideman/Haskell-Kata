e894bdb7a8c1f000a7700df45ecdb1ad59b1d427
yue <MoophyAlan@gmail.com>
2015-09-09 18:29:58 +1200

    ex2 added 	modified:   a3.hs

--- 202/a3/a3.hs
+++ 202/a3/a3.hs
@@ -16,4 +16,15 @@
 --
 --Exercise 2
 --
+--sum2a takes O(1) whereas sum2b and sum2c take O(n), hence sum2a has best
+--run time efficiency.
+--
 sum2a :: Int -> Int
+sum2a n = (div (n * (n + 1)) 2) ^ 2
+sum2b :: Int -> Int
+sum2b n
+  | n == 1    = 1
+  | otherwise = (n ^ 3) + sum2b(n - 1)
+sum2c :: Int -> Int
+sum2c 1 = 1
+sum2c n = (n ^ 3) + sum2b(n - 1)
