d5a6e2b2b4f6e780c525e9dafeb052a403fb35df
yue <MoophyAlan@gmail.com>
2015-09-21 16:26:59 +1200

    a4 2.a done

--- 202/a4/a4.hs
+++ 202/a4/a4.hs
@@ -1,5 +1,27 @@
 --
 -- Exercise 1
---
+-- a
 time  :: Integer -> (Integer, Integer, Integer)
 time i = ((div i 3600), (div (mod i 3600) 60), (mod i 60))
+
+--b
+
+--
+-- Exercise 2
+--a
+type Fraction = (Int, Int)
+
+showFrac :: Fraction -> String
+showFrac frac = "(" ++ show (fst frac) ++ "/" ++ show (snd frac) ++ ")"
+
+makeFrac :: Fraction -> Fraction
+makeFrac frac = (first, second)
+  where 
+    first   =   div (fst frac) common
+    second  =   div (snd frac) common
+    common  =   gcd (fst frac) (snd frac)
+
+powFr :: Fraction -> Int -> String
+powFr b p = showFrac $ makeFrac $ ((fst b) ^ p , ((snd b) ^ p))
+        
+
